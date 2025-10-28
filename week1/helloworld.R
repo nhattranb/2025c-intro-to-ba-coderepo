ede029b816118383c2260253a13de23b4b2b6bfd
nhattranb <s3926629@rmit.edu.vn>
2025-10-28 15:59:08 +0700

    Create new file for Week 1 study

---
+++ week1/helloworld.R
@@ -0,0 +1,15 @@
+print("Hello from the R language")
+
+# Sample variable declaration
+sample <- "Hello from the Business Analytics course"
+print(sample)
+
+# Sample mathematics
+a <- 2025
+b <- 5
+c <- a + b
+d <- a - b
+e <- a*b
+f <- a / b
+max(c, d, e, f)
+
